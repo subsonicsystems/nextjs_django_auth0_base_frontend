dd7f2edc5d4efd2418101263283356e32dda3364
HOSHI Seigo <13637027+subsonicsystems@users.noreply.github.com>
2023-06-25 22:47:34 +0900

    ログインとログアウトを実装

---
+++ pages/api/auth/[...auth0].ts
@@ -0,0 +1,7 @@
+import { handleAuth } from '@auth0/nextjs-auth0';
+
+export default handleAuth({
+  onError(req, res, error) {
+    res.status(error.status || 500).end('Check the console for the error');
+  },
+});
