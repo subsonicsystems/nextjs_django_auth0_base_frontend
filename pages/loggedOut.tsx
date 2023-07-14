d7deaa7fae4e6b394165945a8a0dbbb38c7d8f0b
HOSHI Seigo <13637027+subsonicsystems@users.noreply.github.com>
2023-07-15 00:13:51 +0900

    アロー関数を従来の関数に変更

--- pages/loggedOut.tsx
+++ pages/loggedOut.tsx
@@ -1,25 +1,25 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-const LoggedOut = () => (
-  <>
-    <Head>
-      <title>
-        Nextjs Django Auth0 Base
-      </title>
-    </Head>
-    <main>
-      <div>
+export default function LoggedOut() {
+  return (
+    <>
+      <Head>
+        <title>
+          Nextjs Django Auth0 Base
+        </title>
+      </Head>
+      <main>
+        <div>
         <span>
           ログアウトしました。
         </span>
-        <p />
-        <Link href="/api/auth/login">
-          ログイン
-        </Link>
-      </div>
-    </main>
-  </>
-);
-
-export default LoggedOut;
+          <p />
+          <Link href="/api/auth/login">
+            ログイン
+          </Link>
+        </div>
+      </main>
+    </>
+  );
+}
