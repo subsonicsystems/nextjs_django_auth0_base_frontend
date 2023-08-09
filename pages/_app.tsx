13f07bead0a232672e99011b05d075fbe468bdcd
HOSHI Seigo <13637027+subsonicsystems@users.noreply.github.com>
2023-08-10 03:42:46 +0900

    Material UIとJoy UIを併用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { CssBaseline } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  experimental_extendTheme as materialExtendTheme,
+  THEME_ID as MATERIAL_THEME_ID,
+} from '@mui/material/styles';
+import { CssVarsProvider as JoyCssVarsProvider, extendTheme as joyExtendTheme } from '@mui/joy/styles';
 
-const theme = createTheme({
+const materialTheme = materialExtendTheme({
   typography: {
     fontFamily: '"Yu Gothic",YuGothic,sans-serif',
+  },
+});
+
+const joyTheme = joyExtendTheme({
+  fontFamily: {
+    body: '"Yu Gothic",YuGothic,sans-serif',
   },
 });
 
@@ -14,10 +25,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <UserProvider user={user}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+        <JoyCssVarsProvider theme={joyTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </JoyCssVarsProvider>
+      </MaterialCssVarsProvider>
     </UserProvider>
   );
 }
